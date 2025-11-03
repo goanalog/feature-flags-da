d379c8f4a3025fe7bc7aa630e560165331a600af
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-11-03 12:19:10 -0600

    rebase

---
+++ quickstart/main.tf
@@ -0,0 +1,32 @@
+# Configure the IBM Cloud Provider
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.60"
+    }
+  }
+}
+
+provider "ibm" {
+  # The API key is automatically injected by the IBM Cloud DA service.
+  # We only need to set the region from the user's input.
+  region = var.region
+}
+
+# Find the resource group to deploy into
+data "ibm_resource_group" "group" {
+  name = var.resource_group_name
+}
+
+# Use the official module to create the App Configuration instance
+module "app_config" {
+  source = "terraform-ibm-modules/app-configuration/ibm"
+  version = "1.2.0" # Lock in the version
+  
+  app_config_name   = var.service_name
+  app_config_plan   = "lite" # <--- THIS IS THE FREE LITE PLAN
+  resource_group_id = data.ibm_resource_group.group.id
+  region            = var.region
+  app_config_tags   = ["feature-flags", "lite", "da", "quickstart"]
+}
