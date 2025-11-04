8e6b9c53d154e753d6fca972cc3548fbca194580
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-11-04 09:42:03 -0600

    afd

--- quickstart/outputs.tf
+++ quickstart/outputs.tf
@@ -1,7 +1,15 @@
 output "dashboard_url" {
   description = "Click this link to go directly to your new Feature Flag dashboard."
-  # This is the correct way: Use the module's own URL output.
-  value       = module.app_config.app_config_url
+  
+  # We must manually construct the CRN, as the module does not output it.
+  # CRN format is: crn:v1:bluemix:public:apprapp:<region>:a/<account_id>:<guid>::
+  # We get the account_id from the data.ibm_resource_group.group defined in main.tf
+  value = format(
+    "https://cloud.ibm.com/services/apprapp/%s?paneId=manage",
+    urlencode(
+      "crn:v1:bluemix:public:apprapp:${var.region}:a/${data.ibm_resource_group.group.account_id}:${module.app_config.app_config_guid}::"
+    )
+  )
 }
 
 output "app_config_instance_name" {
