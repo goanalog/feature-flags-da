e6628c6a307678936c902c461a14b142b2d538cb
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-11-04 09:34:21 -0600

    asdf

--- quickstart/outputs.tf
+++ quickstart/outputs.tf
@@ -1,10 +1,10 @@
 output "dashboard_url" {
   description = "Click this link to go directly to your new Feature Flag dashboard."
-  # This is the new, correct URL format using 'apprapp'
+  # This new format uses the CRN (not GUID) and the '/services' path,
+  # matching the working URL format.
   value = format(
-    "https://cloud.ibm.com/apprapp/%s/features?region=%s",
-    module.app_config.app_config_guid,
-    var.region
+    "https://cloud.ibm.com/services/apprapp/%s?paneId=manage",
+    urlencode(module.app_config.app_config_crn)
   )
 }
 
@@ -22,4 +22,10 @@ output "app_config_guid" {
 output "app_config_region" {
   description = "The region of the service instance (used by SDKs)."
   value       = var.region
+}
+
+output "app_config_crn" {
+  description = "The full CRN of the service instance."
+  value       = module.app_config.app_config_crn
+  sensitive   = true
 }
