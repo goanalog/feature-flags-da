af9f2daa4e71a0569f51b8be7b013ef049e06296
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-11-04 09:37:56 -0600

    asdf

--- quickstart/outputs.tf
+++ quickstart/outputs.tf
@@ -1,11 +1,7 @@
 output "dashboard_url" {
   description = "Click this link to go directly to your new Feature Flag dashboard."
-  # This new format uses the CRN (not GUID) and the '/services' path,
-  # matching the working URL format.
-  value = format(
-    "https://cloud.ibm.com/services/apprapp/%s?paneId=manage",
-    urlencode(module.app_config.app_config_crn)
-  )
+  # This is the correct way: Use the module's own URL output.
+  value       = module.app_config.app_config_url
 }
 
 output "app_config_instance_name" {
@@ -22,10 +18,4 @@ output "app_config_guid" {
 output "app_config_region" {
   description = "The region of the service instance (used by SDKs)."
   value       = var.region
-}
-
-output "app_config_crn" {
-  description = "The full CRN of the service instance."
-  value       = module.app_config.app_config_crn
-  sensitive   = true
 }
